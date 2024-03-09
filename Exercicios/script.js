f214778b9bfd34801addbf50d2e0def68d8e1daa
VitorRBrumatti <vitorrufino084@gmail.com>
2024-03-08 23:34:36 -0300

    Commit new add

--- Exercicios/script.js
+++ Exercicios/script.js
@@ -69,22 +69,22 @@ function calculoIniciado() {
     let imc = peso / (altura * altura);
     
     if (imc <= 18.5) {
-        textImc.innerText = `Atenção! Você está abaixo do peso seu imc é ${imc}`
+        textImc.innerText = `Atenção! Você está abaixo do peso seu imc é ${parseInt(imc)}`
     } 
     else if (imc >= 18.6 && imc <= 24.9) {
-        textImc.innerText = `Você está com peso normal e seu imc é ${imc}`
+        textImc.innerText = `Você está com peso normal e seu imc é ${parseInt(imc)}`
     }
     else if (imc >= 25 && imc <= 29.9) {
-        textImc.innerText = `Você está com sobrepeso e seu imc é ${imc}`
+        textImc.innerText = `Você está com sobrepeso e seu imc é ${parseInt(imc)}`
     }
     else if (imc >= 30 && imc <= 34.9) {
-        textImc.innerText = `Você está em Obesidade 1 e seu imc é ${imc}`
+        textImc.innerText = `Você está em Obesidade 1 e seu imc é ${parseInt(imc)}`
     }
     else if (imc >= 35 && imc <= 39.9) {
-        textImc.innerText = `Cuidado! Você está em Obesidade 2 e seu imc é ${imc}`
+        textImc.innerText = `Cuidado! Você está em Obesidade 2 e seu imc é ${parseInt(imc)}`
     }
     else if (imc >= 40 ) {
-        textImc.innerText = `Atenção! Você está em Obesidade 3!! e seu imc é ${imc}`
+        textImc.innerText = `Atenção! Você está em Obesidade 3!! e seu imc é ${parseInt(imc)}`
     }
     else {
         textImc.innerText = `Insira um valor válido!`
